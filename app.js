def6d23c8a779eca3fb92057e1732421be6a205c
Rubia <rj-rosales@hotmail.com>
2023-03-23 23:00:09 -0300

    Funcion eliminar tareas

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
 const btnAdd = document.querySelector(".add");
 const newTask = document.querySelector(".new-task");
-const color = document.querySelector("#color");
+const bColor = document.querySelector("#box-color");
+const fColor = document.querySelector("#font-color");
+const newNameList=document.querySelector("#box-name");
+const nameList=document.querySelector(".name-list");
 const toDo = document.querySelector(".to-do");
+const config = document.querySelector(".config");
+const configBtn = document.querySelector(".config-btn");
 const btnOk = document.querySelector(".ok");
 const ul = document.querySelector(".to-do-list");
 const empty = document.querySelector(".empty");
 
 
+configBtn.addEventListener("click", (e)=>{
+    config.style.display = "block";
+})
+
 btnOk.addEventListener("click", function selectColor() {
-    toDo.style.backgroundColor = color.value;
-    console.log(color.value);
+    toDo.style.backgroundColor = bColor.value;
+    toDo.style.color = fColor.value;
+    config.style.backgroundColor = bColor.value;
+    config.style.color = fColor.value;
+    nameList.textContent=newNameList.value;
+    config.style.display = "none";
 });
 
 
 
 btnAdd.addEventListener("click", (e) => {
     e.preventDefault;
-    li = document.createElement("li");
-    p = document.createElement("p");
-    btnDel = document.createElement("button");
+    if (newTask.value != "") {
+        li = document.createElement("li");
+        p = document.createElement("p");
+        p.textContent = newTask.value;
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+        newTask.value = "";
+        empty.style.display = "none";
+    }
+});
+
+
+
+function addDeleteBtn() {
+    const btnDel = document.createElement("button");
     btnDel.className = "del";
-    btnDel.innerHTML = "-";
-    p.textContent = newTask.value;
-    li.appendChild(p);
-    li.appendChild(btnDel);
-    ul.appendChild(li);
-    newTask.value = "";
-    empty.style.display="none";
+    btnDel.innerHTML = "x";
+    btnDel.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+        const list = document.querySelectorAll("li");
+        if (list.length === 0) {
+            empty.style.display = "block";
+        }
+    });
 
-});
+    return btnDel;
 
-btnDel.addEventListener("click", (e) => {
-    e.preventDefault;
-    li.textContent="";
-});
+}
+
